3808e8b857b9db529e39d87818551539ccaccb9f
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-09-29 17:45:46 -0400

    done

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -2,12 +2,14 @@ import read from './reader.js';
 import json from './parser.js';
 
 export default class GameSavingLoader {
-  static load() {
-    return read()
-      .then((buffer) => json(buffer))
-      .then((data) => {
-        const gameSaving = JSON.parse(data);
-        return gameSaving;
-      });
+  static async load() {
+    try {
+      const buffer = await read();
+      const data = await json(buffer);
+      const gameSaving = JSON.parse(data);
+      return gameSaving;
+    } catch (error) {
+      throw new Error('Failed to load game saving');
+    }
   }
 }
--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -1,20 +1,33 @@
 import GameSavingLoader from '../GameSavingLoader.js';
 
-test('GameSavingLoader.load() should return a Promise', () => {
-  const result = GameSavingLoader.load();
-  expect(result instanceof Promise).toBe(true);
-});
+describe('GameSavingLoader', () => {
+  it('should load a valid GameSaving object', async () => {
+    const saving = await GameSavingLoader.load();
+    expect(saving).toEqual({
+      id: 9,
+      created: 1546300800,
+      userInfo: {
+        id: 1,
+        name: 'Hitman',
+        level: 10,
+        points: 2000,
+      },
+    });
+  });
 
-test('GameSavingLoader.load() should resolve with a valid GameSaving object', async () => {
-  const result = await GameSavingLoader.load();
-  expect(result).toEqual({
-    id: 9,
-    created: 1546300800,
-    userInfo: {
-      id: 1,
-      name: 'Hitman',
-      level: 10,
-      points: 2000,
-    },
+  it('should throw an error if loading fails', async () => {
+    // Создайте mock-функцию, которая будет имитировать ошибку при загрузке
+    const mockRead = jest.fn(() => {
+      throw new Error('Mocked error');
+    });
+
+    // Замените реальную функцию read на mock-функцию
+    jest.mock('./reader', () => ({
+      __esModule: true,
+      default: mockRead,
+    }));
+
+    // Вызовите метод load и ожидайте ошибку
+    await expect(GameSavingLoader.load()).rejects.toThrow('Failed to load game saving');
   });
 });
